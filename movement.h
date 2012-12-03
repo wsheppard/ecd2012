421fb0e719ac450db6fb6b8cc1ddaf4c9b2a7d98
Will Sheppard <sheppard.will@gmail.com>
2012-12-03 17:48:19 +0000

    Basic updates tot he movement module.

--- movement.h
+++ movement.h
@@ -12,6 +12,10 @@
 #define MOVE_LATENCY 100
 #define SERVO_COUNT PWM_COUNT
 #define MOVE_JUMPVAL 1000
+#define SIGMOID_ERR	6.2126
+
+/* ms between steps  - too small isn't going to get scheduled */
+#define MOVE_SIGMOID_LATENCY MOVE_LATENCY
 
 /* Task priority */
 #define MOVE_PRIORITY tskIDLE_PRIORITY 
