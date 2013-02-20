1386bbc1b8d01357a72beee73d8ff3855ee26a74
Will Sheppard <sheppard.will@gmail.com>
2013-02-20 11:58:01 +0000

    Test header exposes the various test functions throughout the project so that you don't have to include the header for the module directly.

---
+++ test.h
@@ -0,0 +1,14 @@
+/*
+ * test.h
+ *
+ *  Created on: 20 Feb 2013
+ *      Author: Will
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+/* Defined in movement.c */
+int test_sigmoid(void);
+
+#endif /* TEST_H_ */
