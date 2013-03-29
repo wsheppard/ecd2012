9e32d14b11e1258b13e9cb8126fe2efdcf68e96b
Rapha <compiaffe@gmail.com>
2013-03-29 19:32:46 +0000

    Update ik.c
    bugfix

--- ik.c
+++ ik.c
@@ -93,7 +93,7 @@ int ik_calc_IK(ik_cart_pos_s position, msg_message_s *msgMessage0, msg_message_s
 	// Check if this position is reachable, if it is not calculate the other possible configuration
 	if((q22 < Q_MIN_1) || q22 > Q_MAX_1 || q32 < Q_MIN_2 || q32 > Q_MAX_2 ){
 		q32 = atan2(+sqrt(1-pow(((pow(xc,2)+pow(yc,2)+pow(zc,2)-pow(l2,2)-pow(l3,2))/(2*l2*l3)),2)),((pow(xc,2)+pow(yc,2)+pow(zc,2)-pow(l2,2)-pow(l3,2))/(2*l2*l3)));
-		q22 = -atan2(((l3*sin(q31))/sqrt(pow(xc,2)+pow(yc,2)+pow(zc,2))),+sqrt(1-pow((l3*sin(q31))/sqrt(pow(xc,2)+pow(yc,2)+pow(zc,2)),2)))+atan2(zc,sqrt(pow(xc,2)+pow(yc,2)));
+		q22 = -atan2(((l3*sin(q32))/sqrt(pow(xc,2)+pow(yc,2)+pow(zc,2))),+sqrt(1-pow((l3*sin(q32))/sqrt(pow(xc,2)+pow(yc,2)+pow(zc,2)),2)))+atan2(zc,sqrt(pow(xc,2)+pow(yc,2)));
 		if((q22 < Q_MIN_1) || q22 > Q_MAX_1 || q32 < Q_MIN_2 || q32 > Q_MAX_2 ){
 			return ECD_ERROR;
 		}
